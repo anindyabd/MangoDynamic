d0a1fc1e915e256f65c4a9bcdd9de84b3fbcdd31
anindyabd <aguha@colgate.edu>
2014-06-26 14:51:41 -0400

    Made the changes suggested by Kevin.

--- db/schema.rb
+++ db/schema.rb
@@ -13,36 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140623155410) do
 
-  create_table "admins", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-
   create_table "campaign_items", force: true do |t|
     t.integer  "duration_id"
     t.integer  "cart_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "campaigns", force: true do |t|
-    t.string   "length"
-    t.string   "integer"
-    t.decimal  "rate"
-    t.float    "discount"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
