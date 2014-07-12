44c2eb238eb3be5eaf73ef348de739074fb2514d
Eric <epv9@case.edu>
2014-07-12 18:06:22 -0400

    Graph Loads Programatically

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
   Morris.Line({
     element: 'click_chart',
-   data: $('#click_chart').data('clicks'),  
-  xkey: 'week',
-  ykeys: 'weibo_clicks',
-  labels: ['click throughs']
+    data: $('#click_chart').data('clicks'),  
+    xkey: 'week',
+    ykeys: ['weibo_clicks'],
+    labels: ['Weibo Clicks'],
+    parseTime: false
+  });
+  Morris.Line({
+    element: 'click_chart2',
+    data: $('#click_chart').data('clicks'),  
+    xkey: 'week',
+    ykeys: ['weibo_clicks'],
+    labels: ['Weibo Clicks'],
+    parseTime: false
+  });
+  Morris.Line({
+    element: 'click_chart3',
+    data: $('#click_chart').data('clicks'),  
+    xkey: 'week',
+    ykeys: ['weibo_clicks'],
+    labels: ['Weibo Clicks'],
+    parseTime: false
   });
 });
