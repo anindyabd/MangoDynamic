472a3c18a33366e1905f1718ac4c82502502ee6d
Eric <epv9@case.edu>
2014-07-16 09:29:09 -0400

    prepare merge

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -3,20 +3,34 @@ $(document).ready(function() {
     element: 'click_chart',
     data: $('#click_chart').data('clicks'),  
     xkey: 'week',
-    ykeys: ['weibo_clicks', 'weibo_followers', 'baidu_clicks'],
-    labels: ['Weibo Clicks', 'Weibo Followers', "Baidu Clicks"],
-    parseTime: false
+    ykeys: ['weibo_clicks', 'baidu_clicks'],
+    labels: ['Weibo Clicks', "Baidu Clicks"],
+    parseTime: false,
+    hideHover: 'never'
   });
+
   Morris.Line({
     element: 'click_chart2',
     data: $('#click_chart2').data('clicks'),  
     xkey: 'week',
     ykeys: ['email'],
     labels: ['Email Signups'],
-    parseTime: false
+    parseTime: false,
+    hideHover: 'never'
   });
-  Morris.Donut({
+
+  Morris.Line({
     element: 'click_chart3',
+    data: $('#click_chart3').data('clicks'),
+    xkey: 'week',
+    ykeys: ['weibo_followers'],
+    labels: ['Weibo Followers'],
+    parseTime: false,
+    hideHover: 'never'
+  });
+
+  Morris.Donut({
+    element: 'pie_chart',
     data: [
       {label: "Clicks Used", value: 40},
       {label: "Clicks Purchased", value: 60}
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,13 +1,11 @@
 module ProfilesHelper
 
-	def weibo_data
+	def clickthrough_data
 		@analytics_by_weeks.map do |w|
 			{
 				week: w.week,
 				weibo_clicks: w.weibo_clicks,
-				weibo_followers: w.weibo_followers,
 				baidu_clicks: w.baidu_clicks
-
 			}
 		end
 	end
@@ -17,6 +15,15 @@ module ProfilesHelper
 			{
 				week: w.week,
 				email: w.email_signups
+			}
+		end
+	end
+
+	def weibo_data
+		@analytics_by_weeks.map do |w|
+			{
+				week: w.week,
+				weibo_followers: w.weibo_followers
 			}
 		end
 	end
